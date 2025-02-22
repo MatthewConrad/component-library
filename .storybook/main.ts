875acb9b53b1f4e94846cf4131e83bba5bb9ad14
Matthew Conrad <conrad.matthewj@gmail.com>
2025-02-22 16:41:30 -0500

    Added storybook's a11y add-on.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,8 +8,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
